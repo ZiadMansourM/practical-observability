19a50fcfe63b41b1f3fe25972656b0c7655e3b4e
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2025-01-27 16:45:05 +0200

    FIX: instrumention of client

--- src/client/client.go
+++ src/client/client.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
+	"go.opentelemetry.io/contrib/bridges/otelslog"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/log/global"
 	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
@@ -19,15 +26,28 @@ import (
 const (
 	serviceName      = "dice-client"
 	otelCollectorURL = "localhost:4318"
+	name             = "github.com/ZiadMansour/bastet/examples/dice"
+)
+
+var (
+	tracer = otel.Tracer(name)
+	// meter  = otel.Meter(name)
+	logger = otelslog.NewLogger(name)
 )
 
 func main() {
-	// Set up OpenTelemetry
-	shutdown := setupOpenTelemetry()
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// Set up OpenTelemetry.
+	otelShutdown, err := setupOTelSDK(ctx)
+	if err != nil {
+		return
+	}
+	// Handle shutdown properly so nothing leaks.
 	defer func() {
-		if err := shutdown(context.Background()); err != nil {
-			log.Fatalf("failed to shut down OpenTelemetry: %v", err)
-		}
+		err = errors.Join(err, otelShutdown(context.Background()))
 	}()
 
 	// Create an instrumented HTTP client
@@ -36,7 +56,7 @@ func main() {
 	}
 
 	// Make 5 requests to the dice server
-	for i := 0; i < 10_000; i++ {
+	for i := 0; i < 5; i++ {
 		callDiceServer(context.Background(), &client)
 		// time.Sleep(500 * time.Millisecond) // Add delay between calls to visualize traces more clearly
 	}
@@ -44,22 +64,21 @@ func main() {
 
 func callDiceServer(ctx context.Context, client *http.Client) {
 	// Start a span for the client request
-	tr := otel.Tracer(serviceName)
-	ctx, span := tr.Start(ctx, "CallDiceServer")
+	ctx, span := tracer.Start(ctx, "CallDiceServer")
 	defer span.End()
 
 	// Make the HTTP request
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://127.0.0.1:8080/rolldice/", nil)
 	if err != nil {
 		span.RecordError(err)
-		log.Printf("Failed to create request: %v\n", err)
+		logger.ErrorContext(ctx, "Failed to create request", "error", err)
 		return
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
 		span.RecordError(err)
-		log.Printf("Request failed: %v\n", err)
+		logger.ErrorContext(ctx, "Request failed", "error", err)
 		return
 	}
 	defer resp.Body.Close()
@@ -68,38 +87,136 @@ func callDiceServer(ctx context.Context, client *http.Client) {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		span.RecordError(err)
-		log.Printf("Failed to read response: %v\n", err)
+		logger.ErrorContext(ctx, "Failed to read response", "error", err)
 		return
 	}
 
-	fmt.Printf("Response: %s\n", body)
+	logger.InfoContext(ctx, fmt.Sprintf("Response: %s", body))
 }
 
-func setupOpenTelemetry() func(context.Context) error {
-	// Set up an OTLP exporter to send traces to the OpenTelemetry Collector
-	exporter, err := otlptracehttp.New(
-		context.Background(),
+// setupOTelSDK bootstraps the OpenTelemetry pipeline.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
+func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	// Set up propagator.
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	// Set up trace provider.
+	tracerProvider, err := newTraceProvider(ctx)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set up meter provider.
+	// meterProvider, err := newMeterProvider()
+	// if err != nil {
+	// 	handleErr(err)
+	// 	return
+	// }
+	// shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	// otel.SetMeterProvider(meterProvider)
+
+	// Set up logger provider.
+	loggerProvider, err := newLoggerProvider()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
+	global.SetLoggerProvider(loggerProvider)
+
+	return
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newTraceProvider(ctx context.Context) (*trace.TracerProvider, error) {
+	// ---> INPUT: context.Context
+	// [1]: Exporter
+	// [2]: Resource
+	// [3]: TracerProvider
+	// ---> OUTPUT: (*trace.TracerProvider, error)
+	traceExporter, err := otlptracehttp.New(
+		ctx,
 		otlptracehttp.WithEndpoint(otelCollectorURL),
 		otlptracehttp.WithInsecure(),
 	)
 
 	if err != nil {
-		log.Fatalf("Failed to create OTLP exporter: %v", err)
+		return nil, err
 	}
 
-	// Create a TracerProvider
-	tp := trace.NewTracerProvider(
-		trace.WithBatcher(exporter),
+	traceProvider := trace.NewTracerProvider(
+		trace.WithBatcher(
+			traceExporter,
+			trace.WithBatchTimeout(time.Second),
+		),
 		trace.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
 			semconv.ServiceName(serviceName),
 		)),
 	)
+	return traceProvider, nil
+}
 
-	// Set the global tracer provider and propagator
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.TraceContext{})
+// func newMeterProvider() (*metric.MeterProvider, error) {
+// 	metricExporter, err := stdoutmetric.New()
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	// Return a shutdown function to clean up
-	return tp.Shutdown
+// 	meterProvider := metric.NewMeterProvider(
+// 		metric.WithReader(metric.NewPeriodicReader(metricExporter,
+// 			// Default is 1m. Set to 3s for demonstrative purposes.
+// 			metric.WithInterval(3*time.Second))),
+// 	)
+// 	return meterProvider, nil
+// }
+
+func newLoggerProvider() (*log.LoggerProvider, error) {
+	logExporter, err := otlploghttp.New(
+		context.Background(),
+		otlploghttp.WithEndpoint(otelCollectorURL),
+		otlploghttp.WithInsecure(),
+	)
+	// logExporter, err := stdoutlog.New()
+	if err != nil {
+		return nil, err
+	}
+
+	loggerProvider := log.NewLoggerProvider(
+		log.WithProcessor(log.NewBatchProcessor(logExporter)),
+		log.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+		)),
+	)
+	return loggerProvider, nil
 }
